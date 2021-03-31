ef62c5aa564219361e984e9eed160f86efee87cf
Juliano Montezano Grams <julianograms@gmail.com>
2021-03-31 15:26:21 -0300

    Changed chat body

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
-import Message from './Messages';
+import React, { Component } from "react";
+import { MessageList as MessageListElements } from "@julianograms/react-chat-elements";
+import "@julianograms/react-chat-elements/dist/main.css";
 
 class MessageList extends Component {
-
   componentDidUpdate(_prevProps, _prevState) {
     this.scrollList.scrollTop = this.scrollList.scrollHeight;
   }
 
-  render () {
+  render() {
     return (
-      <div className="sc-message-list" ref={el => this.scrollList = el}>
-        {this.props.messages.map((message, i) => {
-          return <Message message={message} key={i} />;
-        })}
-      </div>);
+      <div
+        className="sc-message-list"
+        ref={(el) => {
+          this.scrollList = el;
+        }}
+      >
+        <MessageListElements
+          lang="pt_BR"
+          className="message-list"
+          lockable={true}
+          toBottomHeight={"100%"}
+          dataSource={this.props.messages.map((message) => {
+            return {
+              position: message.author === "me" ? "right" : "left",
+              type: "text",
+              text: message.data.text,
+              date: message.time,
+            };
+          })}
+        />
+      </div>
+    );
   }
 }
 
--- src/components/Messages/TextMessage.js
+++ src/components/Messages/TextMessage.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import Linkify from 'react-linkify';
-
+import React from "react";
+import Linkify from "react-linkify";
 
 const TextMessage = (props) => {
-  return <div className="sc-message--text">{
-    <Linkify properties={{ target: '_blank' }}>{props.data.text}</Linkify>
-  }</div>;
+  return (
+    <div className="sc-message--text">
+      {<Linkify properties={{ target: "_blank" }}>{props.data.text}</Linkify>}
+      <div style={{ textAlign: "right" }}>
+        {props.time.getHours()}:{props.time.getMinutes()}
+      </div>
+    </div>
+  );
 };
 
 export default TextMessage;
